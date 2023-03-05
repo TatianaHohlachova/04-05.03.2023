a345179244148f7eceb943dc8c0e5344ef92aaac
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-03-05 10:18:02 +0100

    commit

--- selenium_OOP2.py
+++ selenium_OOP2.py
@@ -12,6 +12,17 @@ time.sleep(0.5)
 page.enter_password('secret_sauce')
 time.sleep(0.5)
 page.click_login()
+
+try:
+    assert driver.current_url == 'https://www.saucedemo.com/inventory.html'
+except AssertionError:
+    print ('Assercja nie przeszła')
+else:
+    print ('Assercja przeszła')
+finally:
+    print ('Po assercji')
+    driver.quit
+
 time.sleep(0.5)
 make_screenshot(driver)
 driver.quit()
